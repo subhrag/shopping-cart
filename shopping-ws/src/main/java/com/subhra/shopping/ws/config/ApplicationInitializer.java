678446c24daacef94ab51a0126160ebb4ad4997a
subhra biswas <subhrab2@gmail.com>
2018-11-11 15:48:17 +0530

    docker compose

--- shopping-ws/src/main/java/com/subhra/shopping/ws/config/ApplicationInitializer.java
+++ shopping-ws/src/main/java/com/subhra/shopping/ws/config/ApplicationInitializer.java
@@ -2,11 +2,16 @@ package com.subhra.shopping.ws.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.subhra.shopping.ws")
-public class ApplicationInitializer {
+public class ApplicationInitializer extends SpringBootServletInitializer{
 
-
+@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApplicationInitializer.class);
+	}
 public static void main(String[] args) {
 	SpringApplication.run(ApplicationInitializer.class, args);
 }	
